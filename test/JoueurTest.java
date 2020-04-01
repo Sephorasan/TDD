dfa6234a59694bcbb063d217132dc6a0fe544b00
Sephora <karkarsephora@gmail.com>
2020-04-01 14:28:29 +0200

    Test Joueur et Plateau

--- test/JoueurTest.java
+++ test/JoueurTest.java
@@ -1,2 +1,28 @@
+import Models.Joueur;
+import Models.Pion;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class JoueurTest {
+
+    @Test
+    public void creerUnPionCorrectTest(){
+        Joueur joueur = new Joueur(Pion.PionType.CROIX);
+        int x = 1;
+        int y = 1;
+        Pion pion = joueur.creerUnPion(x, y);
+        assertEquals(x, pion.posX);
+        assertEquals(y, pion.posY);
+    }
+
+    @Test
+    public void creerUnPionPasCorrectTest(){
+        Joueur joueur = new Joueur(Pion.PionType.CROIX);
+        int x = 1;
+        int y = 1;
+        Pion pion = joueur.creerUnPion(x, y);
+        assertNotEquals(0, pion.posX);
+        assertNotEquals(0, pion.posY);
+    }
 }
--- test/PlateauTest.java
+++ test/PlateauTest.java
@@ -1,3 +1,20 @@
+import Models.Plateau;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PlateauTest {
+
+    @Test
+    public void plateauPleinCorrectTest(){
+        Plateau plateau = new Plateau();
+        assertTrue(plateau.plateauPlein(9));
+    }
+
+    @Test
+    public void plateauPleinPasCorrectTest(){
+        Plateau plateau = new Plateau();
+        assertFalse(plateau.plateauPlein(6));
+    }
+
 
 }
